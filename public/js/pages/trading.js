581679fa7b4660243fffcb75fab6de8313b0e556
Vitence <1026046719@qq.com>
2018-03-01 19:32:57 +0800

    hty kline11

--- public/js/pages/trading.js
+++ public/js/pages/trading.js
@@ -30,7 +30,7 @@ $(function(){
             symbol: "BTC",
             symbolName: "BTC/USD",
             type: "poll", // poll/socket
-            url: "../test.json",
+            url: "../lib/test.json",
             limit: 1000,
             intervalTime: 5000,
             debug: true,
