b4afe1d4fa6f6f18e3060f837cb6deab34612acf
huangshucai <huangsc@makerclass.cn>
2018-03-01 22:07:44 +0800

    单点登录

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -33,11 +33,21 @@ class ControllerBase extends Controller {
      * 初始化用户信息
      */
     public function userInit(){
-        $userInfo = $this->session->get('userInfo');
-        if ($userInfo){
-            $this->userInfo = $userInfo;
+        $cookie = $_COOKIE['EXCHANGE'];
+        $user = json_decode(base64_decode($cookie),true);
+        if(!empty($user)){
+            $user = ExUsers::itemById($user['id']);
+            if(empty($user)){
+                $this->session->remove('userInfo');
+            }else{
+                $this->userInfo = $user;
+                $this->session->set('userInfo',$user);
+                $this->view->setVar('userInfo',$this->userInfo);
+                $this->view->setVar('mycookie',$cookie);
+            }
+        }else{
+            $this->session->remove('userInfo');
         }
-        $this->view->setVar('userInfo',$this->userInfo);
     }
     
     
@@ -69,10 +79,12 @@ class ControllerBase extends Controller {
     public function saveSession($user){
         unset($user['password']);
         unset($user['trading_password']);
+        setcookie('EXCHANGE',base64_encode(json_encode($user)),time()+60*60,'/','xinhuodata.com');
         $this->session->set('userInfo',$user);
     }
     
     public function deleteSession(){
+        setcookie('EXCHANGE','',time() - 60*60,'/','xinhuodata.com');
         $this->session->destroy();
     }
 }
